83cbcf4864df035f81b17276eef575f6b69195e3
binbin <binbinyouli_2010@163.com>
2022-09-05 21:30:22 +0800

    vue3项目初始化

---
+++ src/components/demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>helloword</p>
+  <div class="col"></div>
+  <img :src="dev33" />
+  <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+</template>
+
+<script>
+
+    import img33 from "@/assets/img/1.jpg"
+export default {
+    data(){
+        return {
+            dev33: img33
+        }
+    }
+}
+</script>
+
+<style>
+@import url("@/assets/css/demo.css");
+</style>
