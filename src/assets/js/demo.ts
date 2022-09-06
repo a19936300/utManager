37b5308ce2c930c549cfb8be790cc6289df72ebc
bin-jixie <bin-jixie>
2022-09-06 18:13:18 +0800

    修改使用ts

---
+++ src/assets/js/demo.ts
@@ -0,0 +1,7 @@
+function demofu(person) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+document.body.innerHTML = demofu(user);
--- src/components/demo.vue
+++ src/components/demo.vue
@@ -3,23 +3,22 @@
   <div class="col"></div>
   <img :src="dev33" />
   <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
 </template>
 
 <script>
-
-    import img33 from "@/assets/img/1.jpg"
+import img33 from "@/assets/img/1.jpg";
 export default {
-    data(){
-        return {
-            dev33: img33
-        }
-    }
-}
+  data() {
+    return {
+      dev33: img33,
+    };
+  },
+};
 </script>
 
 <style>
