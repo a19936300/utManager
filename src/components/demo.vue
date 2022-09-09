e6f8dd2541b12ae026e60dd1e00f27dc627dea99
bin-jixie <bin-jixie>
2022-09-09 09:31:10 +0800

    typescript study

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -3,24 +3,33 @@
   <div class="col"></div>
   <img :src="dev33" />
   <el-button>Default</el-button>
-  <el-button type="primary">Primary</el-button>
+  <el-button @click="demoClik" type="primary">Primary</el-button>
   <el-button type="success">Success</el-button>
   <el-button type="info">Info</el-button>
   <el-button type="warning">Warning</el-button>
   <el-button type="danger">Danger</el-button>
 </template>
 
-<script>
-import img33 from "@/assets/img/1.jpg";
+<script lang="ts">
+import {pingGuo} from "@assets/js/shuiguo"
+import img33 from "@assets/img/1.jpg";
+
 export default {
   data() {
     return {
       dev33: img33,
     };
   },
+  methods: {
+    demoClik(){
+    var pingguo = new pingGuo();
+    console.log(pingguo.kouWei())
+  }
+  }
+  
 };
 </script>
 
 <style>
-@import url("@/assets/css/demo.css");
+@import url("@assets/css/demo.css");
 </style>
