9777b60f6c87eb6505e383681314623f42185b39
binbin <binbinyouli_2010@163.com>
2022-09-19 21:35:50 +0800

    zhangfei

---
+++ src/assets/js/打卡/2022年9月19日.ts
@@ -0,0 +1,14 @@
+export class yuansu {
+
+}
+
+let tuple_name = ["fsdf",1223]
+
+let tuple_code = []
+
+tuple_code[0] = "1212";
+tuple_code[1] = 123123;
+
+tuple_code.push("sdfsd");
+tuple_code.pop();
+
