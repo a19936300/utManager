0df54152c5e674d06bd8c03d88a4b593f852edd3
binbin <binbinyouli_2010@163.com>
2022-09-24 23:16:16 +0800

    typescript

--- src/assets/js/打卡/2022年9月19日.ts
+++ src/assets/js/打卡/2022年9月19日.ts
@@ -12,3 +12,10 @@ tuple_code[1] = 123123;
 tuple_code.push("sdfsd");
 tuple_code.pop();
 
+
+
+// 365天不间断打卡
+
+/**
+
+ */
---
+++ src/assets/js/打卡/2022年9月24日.ts
@@ -0,0 +1,269 @@
+{
+    "id": "370084468eb82085",
+    "topic": "TypeScript",
+    "root": true,
+    "children": [
+        {
+            "topic": "语言概览",
+            "id": "3700889e8c1ef125",
+            "expanded": true,
+            "children": [
+                {
+                    "topic": "变量",
+                    "id": "37009a8550bb5d2e"
+                },
+                {
+                    "topic": "注释",
+                    "id": "37009b931dc73e16"
+                },
+                {
+                    "topic": "字面量",
+                    "id": "37009c210ca7d2c3"
+                },
+                {
+                    "topic": "数组",
+                    "id": "37009cfde663ac2e"
+                },
+                {
+                    "topic": "函数",
+                    "id": "37009dc3dae34a80"
+                },
+                {
+                    "topic": "Bigint",
+                    "id": "37009e6b4190cfda"
+                },
+                {
+                    "topic": "展开运算符",
+                    "id": "37009fec22062bd2"
+                },
+                {
+                    "topic": "结构",
+                    "id": "3700a1542be89470"
+                },
+                {
+                    "topic": "可选链运算符",
+                    "id": "3700a249c24cc6b7"
+                },
+                {
+                    "topic": "空值合并运算符",
+                    "id": "3700a4ad61cc5bd0"
+                }
+            ],
+            "direction": 0
+        },
+        {
+            "topic": "类型系统",
+            "id": "37008dbfe199037d",
+            "expanded": true,
+            "children": [
+                {
+                    "topic": "类型基础",
+                    "id": "3700ad8280ddbeb7",
+                    "expanded": true,
+                    "children": [
+                        {
+                            "topic": "类型注解",
+                            "id": "3700aec14648ab38"
+                        },
+                        {
+                            "topic": "类型检查",
+                            "id": "3700affa743aaec5"
+                        },
+                        {
+                            "topic": "原始类型",
+                            "id": "3700b0ec5afeeaf1"
+                        },
+                        {
+                            "topic": "枚举类型",
+                            "id": "3700b2286a464509"
+                        },
+                        {
+                            "topic": "字面量类型",
+                            "id": "3700b36cbc969ed8"
+                        },
+                        {
+                            "topic": "单元类型",
+                            "id": "3700b4bc45c2eac6"
+                        },
+                        {
+                            "topic": "顶端类型",
+                            "id": "3700b604d8eb83a3"
+                        },
+                        {
+                            "topic": "尾端类型",
+                            "id": "3700b710611bde18"
+                        },
+                        {
+                            "topic": "元元组类型",
+                            "id": "3700b927892a60a1"
+                        },
+                        {
+                            "topic": "对象类型",
+                            "id": "3700bb5bffe90e6f"
+                        },
+                        {
+                            "topic": "函数类型",
+                            "id": "3700bc4c4834ca5d"
+                        },
+                        {
+                            "topic": "接口",
+                            "id": "3700bd5705ec8204"
+                        },
+                        {
+                            "topic": "类型别名",
+                            "id": "3700be12887b35eb"
+                        },
+                        {
+                            "topic": "类",
+                            "id": "3700bffa9eb430a6"
+                        }
+                    ]
+                },
+                {
+                    "topic": "类型进阶",
+                    "id": "3700c2e9725814a9",
+                    "expanded": true,
+                    "children": [
+                        {
+                            "topic": "泛型",
+                            "id": "3700ca7a67184197"
+                        },
+                        {
+                            "topic": "局部类型",
+                            "id": "3700cb3db1cf6909"
+                        },
+                        {
+                            "topic": "联合类型",
+                            "id": "3700cc8802c40ae8"
+                        },
+                        {
+                            "topic": "交叉类型",
+                            "id": "3700cd999d1539bc"
+                        },
+                        {
+                            "topic": "索引类型",
+                            "id": "3700ce55e1d5bebc"
+                        },
+                        {
+                            "topic": "条件类型",
+                            "id": "3700cf8abaeb8b6c"
+                        },
+                        {
+                            "topic": "内置工具类型",
+                            "id": "3700d09dd3707a50"
+                        },
+                        {
+                            "topic": "类型查询",
+                            "id": "3700d24991735ad1"
+                        },
+                        {
+                            "topic": "类型断言",
+                            "id": "3700d32fe1cb7fc7"
+                        },
+                        {
+                            "topic": "类型细化",
+                            "id": "3700d45e5e74c95d"
+                        }
+                    ]
+                },
+                {
+                    "topic": "类型深入",
+                    "id": "3700d684488a7fae",
+                    "expanded": true,
+                    "children": [
+                        {
+                            "topic": "子类型兼容型",
+                            "id": "3700dd5595d13f26"
+                        },
+                        {
+                            "topic": "赋值兼容性",
+                            "id": "3700dfa0c14cc8b8"
+                        },
+                        {
+                            "topic": "类型推断",
+                            "id": "3700e2c6864548c5"
+                        },
+                        {
+                            "topic": "类型放宽",
+                            "id": "3700e4153a3eeb5a"
+                        },
+                        {
+                            "topic": "命名空间",
+                            "id": "3700e5e652204d5c"
+                        },
+                        {
+                            "topic": "模块",
+                            "id": "3700e71b661dbdbc"
+                        },
+                        {
+                            "topic": "外部声明",
+                            "id": "3700e7c79d7c31f2"
+                        },
+                        {
+                            "topic": "使用声明文件",
+                            "id": "3700e968452acc3c"
+                        },
+                        {
+                            "topic": "模块解析",
+                            "id": "3700eae37b85fba7"
+                        },
+                        {
+                            "topic": "声明合并",
+                            "id": "3700ebc41bc587aa"
+                        }
+                    ]
+                }
+            ],
+            "direction": 1
+        },
+        {
+            "topic": "应用",
+            "id": "37009100967a00b7",
+            "expanded": true,
+            "children": [
+                {
+                    "topic": "编译器",
+                    "id": "3700f7a5593c7212"
+                },
+                {
+                    "topic": "编译选项",
+                    "id": "3700f8760f77a27b"
+                },
+                {
+                    "topic": "tsconfig.json",
+                    "id": "3700f97f9c6df874"
+                },
+                {
+                    "topic": "工程引用",
+                    "id": "3700fb676b290d50"
+                },
+                {
+                    "topic": "javaScript类型检查",
+                    "id": "3700fd4f363e1be2"
+                },
+                {
+                    "topic": "三斜杠命令",
+                    "id": "3700ff5f3658e6d6"
+                },
+                {
+                    "topic": "Babel",
+                    "id": "370102428a2d6bf4"
+                },
+                {
+                    "topic": "webpack",
+                    "id": "3701033b98d25ce9"
+                },
+                {
+                    "topic": "Eslint",
+                    "id": "3701040a523af0e1"
+                },
+                {
+                    "topic": "Visual Studio code",
+                    "id": "3701052ab76de09d"
+                }
+            ],
+            "direction": 0
+        }
+    ],
+    "expanded": true
+}
